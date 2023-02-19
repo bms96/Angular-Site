4df61fd259210f011d2fed0c741e1db053dedcad
bms96 <65141136+bms96@users.noreply.github.com>
2023-02-18 20:43:07 -0500

    added some of the todo functionality where creating a new todo works. delete has been started and will need to work on edit

--- angularWebsite/src/app/main-page/todo/todo.component.ts
+++ angularWebsite/src/app/main-page/todo/todo.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
 export class TodoComponent {
 
   todos: any = [{"id":"1","name":"Pete", "age":"32"}, {"id":"2","name":"ManBearPig", "age":"??"}, {"id":"3","name":"Bryan", "age":"26"}];
+  nextId: number = this.todos.length + 1;
 
+  createTodo() {
+    this.todos.push({"id": this.nextId, "name":"testname", "age":"20"})
+    this.nextId +=1;
+  }
+
+  deleteTodo(id: number) {
+    for (let todo of this.todos) {
+      console.log(todo.name);
+    }
+  }
 }
